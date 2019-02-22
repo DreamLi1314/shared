973121d9f142c7b0eb4a6684cf8926d299cb42fb
DreamLi <18829346477@163.com>
2019-02-22 16:00:25 +0800

    code review

--- java/v1/javaChat/src/com/inetsoft/main/ClientMain.java
+++ java/v1/javaChat/src/com/inetsoft/main/ClientMain.java
@@ -4,6 +4,6 @@ import com.inetsoft.client.Client;
 
 public class ClientMain {
     public static void main(String[] args) throws Exception {
-        new Client().connect("127.0.0.1", 9999);
+        new Client().connect(Main.CHAT_HOST, Main.CHAT_PORT);
     }
 }
--- java/v1/javaChat/src/com/inetsoft/main/Main.java
+++ java/v1/javaChat/src/com/inetsoft/main/Main.java
@@ -3,7 +3,12 @@ package com.inetsoft.main;
 import com.inetsoft.server.Server;
 
 public class Main {
+
+    public static final String CHAT_HOST = "127.0.0.1";
+    public static final int CHAT_PORT = 9999;
+
+
     public static void main(String[] args) throws Exception {
-        new Server().serverStart(9999);
+        new Server().serverStart(CHAT_PORT);
     }
 }
