db3c68d7607ec017d7105eac42db0d0700baca31
DreamLi <18829346477@163.com>
2019-03-18 17:49:29 +0800

    merge edit page and add employee page

--- java/v1/sb-crud/src/main/java/com/dreamli/crud/controller/EmployeeController.java
+++ java/v1/sb-crud/src/main/java/com/dreamli/crud/controller/EmployeeController.java
@@ -9,7 +9,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 import java.util.Map;
@@ -46,6 +49,26 @@ public class EmployeeController {
 
         // 跳转到添加员工页面
         return "emp/add";
+    }
+
+    @PostMapping("/emp")
+    public String addEmp(Employee emp) {
+        LOGGER.debug("add employee: " + emp);
+        // 校验数据, 添加员工
+        if(!StringUtils.isEmpty(emp.getLastName()) && !StringUtils.isEmpty(emp.getDepartment())) {
+            employeeDao.save(emp);
+        }
+
+        // 跳转到员工列表页面显示所有员工
+        return "redirect:/emps";
+    }
+
+    @GetMapping("/emp/{id}")
+    public String getEditEmp(@PathVariable Integer id, Model model) {
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("editEmp", employee);
+
+        return toAddPage(model);
     }
 
     private final DepartmentDao departmentDao;
