48a7de2d20ae5e4b17610089dc73bcd0a5515f45
DreamLi <18829346477@163.com>
2019-10-31 22:42:19 +0800

    code review.

--- java/v1/sb-dubbo/provider-ticket/src/main/java/com/jack/ticket/ProviderTicketApplication.java
+++ java/v1/sb-dubbo/provider-ticket/src/main/java/com/jack/ticket/ProviderTicketApplication.java
@@ -1,9 +1,11 @@
 package com.jack.ticket;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubbo
 public class ProviderTicketApplication {
 
     public static void main(String[] args) {
