7de721eb6244e7510388541d193a386844df1016
Jack Li <18829346477@163.com>
2020-04-26 23:53:06 +0800

    add model.

--- java/v3/pushweather/src/main/java/org/javafamily/model/RealTime.java
+++ java/v3/pushweather/src/main/java/org/javafamily/model/RealTime.java
@@ -6,9 +6,11 @@ public class RealTime implements Serializable {
    private String date; // 测量日期
    private String time; // 测量时间
    private String week; // 星期
-   private String city; // 城市
-   private String lunar; // 农历
-   private RealTimeWeather realTimeWeather; // 实时天气情况
+   @JsonProperty("city_name")
+   private String cityName; // 城市
+   private String moon; // 农历
+   private Weather weather; // 实时天气情况
+   private Wind wind; // 风况
 
    public String getDate() {
       return date;
@@ -34,31 +36,78 @@ public class RealTime implements Serializable {
       this.week = week;
    }
 
-   public String getCity() {
-      return city;
+   public String getCityName() {
+      return cityName;
    }
 
-   public void setCity(String city) {
-      this.city = city;
+   public void setCityName(String cityName) {
+      this.cityName = cityName;
    }
 
-   public String getLunar() {
-      return lunar;
+   public String getMoon() {
+      return moon;
    }
 
-   public void setLunar(String lunar) {
-      this.lunar = lunar;
+   public void setMoon(String moon) {
+      this.moon = moon;
    }
 
-   public RealTimeWeather getRealTimeWeather() {
-      return realTimeWeather;
+   public Weather getWeather() {
+      return weather;
    }
 
-   public void setRealTimeWeather(RealTimeWeather realTimeWeather) {
-      this.realTimeWeather = realTimeWeather;
+   public void setWeather(Weather weather) {
+      this.weather = weather;
    }
 
-   class RealTimeWeather {
+   public Wind getWind() {
+      return wind;
+   }
+
+   public void setWind(Wind wind) {
+      this.wind = wind;
+   }
+
+   class Wind implements Serializable {
+      private String direct; // 风向
+      private String power; // 风力
+      private String windspeed; // 风速
+      private String offset; // 偏移
+
+      public String getDirect() {
+         return direct;
+      }
+
+      public void setDirect(String direct) {
+         this.direct = direct;
+      }
+
+      public String getPower() {
+         return power;
+      }
+
+      public void setPower(String power) {
+         this.power = power;
+      }
+
+      public String getWindspeed() {
+         return windspeed;
+      }
+
+      public void setWindspeed(String windspeed) {
+         this.windspeed = windspeed;
+      }
+
+      public String getOffset() {
+         return offset;
+      }
+
+      public void setOffset(String offset) {
+         this.offset = offset;
+      }
+   }
+
+   class Weather implements Serializable {
       private String humidity; // 湿度
       private String temperature; // 温度
       private String info; // 天气情况
