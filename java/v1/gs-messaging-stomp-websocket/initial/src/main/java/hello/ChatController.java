f996aac26bf26d0e4263994f7a563bbc07bb0179
DreamLi <18829346477@163.com>
2020-11-06 07:38:33 +0800

    update

--- java/v1/gs-messaging-stomp-websocket/initial/src/main/java/hello/ChatController.java
+++ java/v1/gs-messaging-stomp-websocket/initial/src/main/java/hello/ChatController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
 @Controller
-public class GreetingController {
+public class ChatController {
 
 	@MessageMapping("/all") // 指定映射地址, 前端发送 message 时全地址为 prefix + mapping
 	@SendTo("/topic/broadcast") // 指定订阅地址, 该方法返回的数据所有订阅该地址的客户端都会收到
